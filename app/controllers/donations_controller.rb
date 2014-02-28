811644d7690d4fbcb0643af1f0833b8303ba3175
Shawn Oakley <shawnoakley@gmail.com>
2014-02-28 01:40:20 -0500

    Physical items now displaying on user show view.

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -11,7 +11,19 @@ class DonationsController < ApplicationController
 		@donation.user_id = @user_id
 		if @donation.save
 			new_params = {type: params[:donation_type], id: instance.id}
-			redirect_to user_donation_path(new_params)
+			# fail
+			params[:id] = params[:current_user_id]
+			case params[:donation_type]
+			when 'Voucher'
+				@voucher = instance
+				render :template => 'donations/voucher/show'
+			when 'Experience'
+				@experience = instance
+				render :template => 'donations/experience/show'
+			when 'Physical Item'
+				@physical_item = instance
+				render :template => 'donations/physicalItem/show'
+			end
 		else
 			fail
 			# flash error
@@ -22,9 +34,8 @@ class DonationsController < ApplicationController
 
 		@donation = Donation.new
 		@user_id = params[:current_user_id]
-
 		case params[:donation_type]
-		when 'Voucher'
+		when 'Voucher' || 'voucher'
 			@voucher = Voucher.new(params[:donation])
 			if @voucher.save
 				save_polymorphic_item(@voucher)	
@@ -32,7 +43,7 @@ class DonationsController < ApplicationController
 				fail
 				# flash error
 			end
-		when 'Experience'
+		when 'Experience' || 'experience'
 			@experience = Experience.new(params[:donation])
 			if @experience.save
 				save_polymorphic_item(@experience)
@@ -40,9 +51,9 @@ class DonationsController < ApplicationController
 				fail
 				# flash error
 			end
-		when 'physical_item'
-			params[:donation][:donatable_type] = "Physical Item"
-			params[:donation_type] = "Physical Item"
+		when 'physical_item' || 'Physical Item'
+			params[:donation][:donatable_type] = "PhysicalItem"
+			params[:donation_type] = "PhysicalItem"
 			@physical_item = PhysicalItem.new(params[:donation])
 			if @physical_item.save
 				save_polymorphic_item(@physical_item)
@@ -54,14 +65,15 @@ class DonationsController < ApplicationController
 	end
 
 	def show
+		# fail
 		case params[:type]
-		when 'voucher'
+		when 'voucher' || 'Voucher'
 			@voucher = Voucher.find_by_id(params[:id])
 			render :template => 'donations/voucher/show'
-		when 'experience'
+		when 'experience' || 'Experience'
 			@experience = Experience.find_by_id(params[:id])
 			render :template => 'donations/experience/show'
-		when 'physical_item'
+		when 'physical_item' || 'Physical Item'
 			@physical_item = PhysicalItem.find_by_id(params[:id])
 			render :template => 'donations/physicalItem/show'
 		end
